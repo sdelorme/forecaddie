5a4d7dc1bdf67be5236c295cf8f280150a7adade
Seth Delorme <seth.delorme@gambyt.com>
2024-01-11 23:31:26 -0500

    add fetch for events, yay

--- src/app/events/FetchEvents.tsx
+++ src/app/events/FetchEvents.tsx
@@ -1,7 +1,17 @@
-async function getEvemts() {
-  // const response = await fetch()
+async function getEvents() {
+  const response = await fetch(
+    `https://feeds.datagolf.com/get-schedule?tour=pga&file_format=json&key=${process.env.DATA_GOLF_API_KEY}`
+  )
+  return response.json()
 }
 
 export default async function FetchEvents() {
-  return <div>fetchEvents</div>
+  const schedule = await getEvents()
+  return (
+    <>
+      {schedule.schedule.map((event: { [key: string]: string }) => (
+        <div key={event.event_id}>{event.event_name}</div>
+      ))}
+    </>
+  )
 }
--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
+import FetchEvents from './FetchEvents'
 
 export default function EventsPage() {
   return (
     <main>
       <h2>Heller this is the events page</h2>
       <Link href="/">Back to home Page</Link>
+      <FetchEvents />
     </main>
   )
 }
