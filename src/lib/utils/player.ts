a565cac100917d56abf6dc07084b9615b394f63f
Seth Delorme <seth.delorme@gambyt.com>
2025-03-07 21:49:28 -0500

    husky pre commit fix

--- src/lib/utils/player.ts
+++ src/lib/utils/player.ts
@@ -2,27 +2,19 @@ import type { LeaderboardPlayer } from '@/types/leaderboard'
 import type { Player } from '@/types/player'
 
 export function formatLeaderboardPlayerName(player: Pick<LeaderboardPlayer, 'playerName'>) {
-  if (!player?.playerName) return ''
   const [lastName, firstName] = player.playerName.split(', ')
-  if (!lastName || !firstName) return player.playerName
   return `${firstName[0]}. ${lastName}`
 }
 
 export function formatPlayerListName(player: Pick<Player, 'playerName'>) {
-  if (!player?.playerName) return ''
   const [lastName, firstName] = player.playerName.split(', ')
-  if (!lastName || !firstName) return player.playerName
   return `${firstName} ${lastName}`
 }
 
 export function getLastName(playerName: string) {
-  if (!playerName) return ''
-  const parts = playerName.split(', ')
-  return parts[0] || playerName
+  return playerName.split(', ')[0]
 }
 
 export function getFirstLetterOfLastName(playerName: string) {
-  if (!playerName) return ''
-  const lastName = getLastName(playerName)
-  return lastName.charAt(0).toUpperCase()
+  return getLastName(playerName).charAt(0).toUpperCase()
 }
